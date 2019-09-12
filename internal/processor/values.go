e6badfffb91ba4aeefdebd5f328dbb4ce9281274
hren@newrelic.com <hren@newrelic.com>
2019-09-12 14:46:56 +1000

    Updated RunValueParser-> for float numbers, use decimal point format instead of scientific notation

--- internal/processor/values.go
+++ internal/processor/values.go
@@ -56,7 +56,15 @@ func RunSubParse(subParse []load.Parse, currentSample *map[string]interface{}, k
 func RunValueParser(v *interface{}, api load.API, key *string) {
 	for regexKey, regexVal := range api.ValueParser {
 		if formatter.KvFinder("regex", *key, regexKey) {
-			value := fmt.Sprintf("%v", *v)
+			var value interface{}
+			switch (*v).(type) {
+			case float32, float64:
+				// For float numbers, use decimal point format instead of scientific notation for large exponents(e.g. 2026112.000000 vs 2.026112e+06 )
+				// to allow parser to process the original float number 2026112.000000 rather 2.026112e+06
+				value = fmt.Sprintf("%f", *v)
+			default:
+				value = fmt.Sprintf("%v", *v)
+			}
 			*v = formatter.ValueParse(value, regexVal)
 		}
 	}
