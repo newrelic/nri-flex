3562b668525458bbea8953ce7055b53611dcc023
Kav91 <kav.pather@gmail.com>
2020-01-01 22:50:20 +1100

    update linter

--- .buildkite/_ci_steps/test.sh
+++ .buildkite/_ci_steps/test.sh
@@ -2,10 +2,12 @@
 export CGO_ENABLED=0
 
 echo -e "--- \033[33m Grabbing Dependencies \033[0m :golang::flying_saucer:"
+dep version
 dep ensure -v
 
 echo -e "--- \033[33m Running Tests \033[0m :golang::hammer_and_wrench:"
 go test -v -coverprofile=coverage.txt -covermode=atomic ./...
 
 echo -e "--- \033[33m Running Linter \033[0m :golang::lint-remover:"
+golangci-lint --version
 golangci-lint run -v
