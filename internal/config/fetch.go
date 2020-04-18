888a7f4d034465072cde389820758b092c7568d2
Kav91 <kav.pather@gmail.com>
2020-04-18 19:44:39 +1000

    chore: remove unneeded print [skip ci]

--- internal/config/fetch.go
+++ internal/config/fetch.go
@@ -198,7 +198,6 @@ func FetchLookups(cfg *load.Config, apiNo int, samplesToMerge *load.SamplesToMer
 				"name": cfg.Name,
 				"err":  err,
 			}).Error("fetch: failed to unmarshal lookup config")
-			fmt.Println(newAPI)
 		} else {
 			lookupConfig.APIs = append(lookupConfig.APIs, API)
 		}
