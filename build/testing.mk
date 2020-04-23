96b8bfc8195df126f3de8087e0dabd541deb17a4
Antonio Dias <adias@newrelic.com>
2020-04-23 17:05:09 +0200

    remove integration testing from unit testing

--- build/testing.mk
+++ build/testing.mk
@@ -9,14 +9,14 @@ lint: deps
 	@$(GOLINTER) run ./...
 
 test: test-only
-test-only: test-unit test-integration
+test-only: test-unit
 
 test-unit:
 	@echo "=== $(PROJECT_NAME) === [ unit-test        ]: running unit tests..."
 	@mkdir -p $(COVERAGE_DIR)
 	@$(GO_CMD) test -tags unit -covermode=$(COVERMODE) -coverprofile $(COVERAGE_DIR)/unit.tmp $(GO_PKGS)
 
-test-integration:
+test-integration: test-unit
 	@echo "=== $(PROJECT_NAME) === [ integration-test ]: running integration tests..."
 	@mkdir -p $(COVERAGE_DIR)
 	@sh ./integration-test/ci-test.sh
