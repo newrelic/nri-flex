6444959e6218a9fb7d7ce41019877c8d69ce321b
Antonio Dias <adias@newrelic.com>
2020-04-27 16:36:45 +0200

    add windows command line test removed "shell" in windows. doesnt work properly

--- integration-test/cmd_windows_test.go
+++ integration-test/cmd_windows_test.go
@@ -7,24 +7,15 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/newrelic/infra-integrations-sdk/integration"
-
 	//"github.com/newrelic/nri-flex/integration-test/integration"
 	"github.com/newrelic/nri-flex/internal/config"
 	"github.com/newrelic/nri-flex/internal/load"
-
 	"github.com/stretchr/testify/require"
 )
 
 var flexMain = filepath.Join("..", "cmd", "nri-flex", "nri-flex.go")
 
 func Test_WindowsCommands_ReturnsData(t *testing.T) {
-	// given
-	load.Refresh()
-
-	i, _ := integration.New(load.IntegrationName, load.IntegrationVersion)
-	load.Entity, _ = i.Entity("IntegrationTest", "nri-flex")
-
 	// Load a single config file
 	var configs []load.Config
 	configFile, _ := os.Stat(filepath.Join("configs", "windows-cmd-test.yml"))
