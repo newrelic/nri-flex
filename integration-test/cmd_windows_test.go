8c2113ad0ad69a37cf8cc93d7d80b3ad23eb8211
Antonio Dias <adias@newrelic.com>
2020-04-28 18:29:57 +0200

    move windows configs to windows folder properly test windows command results

--- integration-test/cmd_windows_test.go
+++ integration-test/cmd_windows_test.go
@@ -13,6 +13,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var configDirPath = filepath.Join("configs", "windows")
+
 func Test_WindowsCommands_ReturnsData(t *testing.T) {
 	configDirPath := filepath.Join("configs", "windows")
 
@@ -23,6 +25,44 @@ func Test_WindowsCommands_ReturnsData(t *testing.T) {
 
 	// set file to load
 	load.Args.ConfigFile = filepath.Join(configDirPath, "windows-cmd-test.yml")
+
+	// when
+	err := integration.RunFlex(integration.FlexModeDefault)
+	require.NoError(t, err)
+
+	//then
+	metricsSet := load.Entity.Metrics
+	require.NotEmpty(t, metricsSet)
+
+	for _, ms := range metricsSet {
+		if ms.Metrics["event_type"] == "flexStatusSample" {
+			continue
+		}
+		require.NotNil(t, ms.Metrics["status"], "status")
+		require.NotNil(t, ms.Metrics["name"], "name")
+		require.NotNil(t, ms.Metrics["displayname"], "displayname")
+	}
+
+	// check for a specific service, because Flex ingests everything, even output "garbage"
+	// any Windows version should always have the Themes service, so check for that
+	var found bool
+	for _, ms := range metricsSet {
+		if ms.Metrics["name"] == "Themes" {
+			found = true
+		}
+	}
+
+	require.Truef(t, found, "didn't find the 'Themes' service. check that the configuration is correct")
+}
+
+func Test_WindowsCommands_ConfigsFolder(t *testing.T) {
+	load.Refresh()
+
+	i, _ := sdk.New(load.IntegrationName, load.IntegrationVersion)
+	load.Entity, _ = i.Entity("IntegrationTest", "nri-flex")
+
+	// set configurations folder
+	load.Args.ConfigDir = configDirPath
 
 	// when
 	err := integration.RunFlex(integration.FlexModeDefault)
