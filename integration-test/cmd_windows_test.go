3e0c4320f04aa47ed25664486f1b8f6a668bfff7
Antonio Dias <adias@newrelic.com>
2020-04-28 18:29:57 +0200

    move windows configs to windows folder properly test windows command results

--- integration-test/cmd_windows_test.go
+++ integration-test/cmd_windows_test.go
@@ -3,39 +3,41 @@
 package integration
 
 import (
-	"os"
 	"path/filepath"
 	"testing"
 
-	"github.com/newrelic/infra-integrations-sdk/integration"
-	"github.com/newrelic/nri-flex/internal/config"
+	sdk "github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/newrelic/nri-flex/internal/integration"
 	"github.com/newrelic/nri-flex/internal/load"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_WindowsCommands_ReturnsData(t *testing.T) {
+	configDirPath := filepath.Join("configs", "windows")
+
 	load.Refresh()
 
-	i, _ := integration.New(load.IntegrationName, load.IntegrationVersion)
+	i, _ := sdk.New(load.IntegrationName, load.IntegrationVersion)
 	load.Entity, _ = i.Entity("IntegrationTest", "nri-flex")
 
-	// Load a single config file
-	var configs []load.Config
-	configFile, _ := os.Stat(filepath.Join("configs", "windows-cmd-test.yml"))
-	err := config.LoadFile(&configs, configFile, "configs")
-	require.NoError(t, err)
+	// set file to load
+	load.Args.ConfigFile = filepath.Join(configDirPath, "windows-cmd-test.yml")
 
 	// when
-	errs := config.RunFiles(&configs)
-	require.Empty(t, errs)
+	err := integration.RunFlex(integration.FlexModeDefault)
+	require.NoError(t, err)
 
+	//then
 	metricsSet := load.Entity.Metrics
 	require.NotEmpty(t, metricsSet)
 
 	for _, ms := range metricsSet {
-		require.NotNil(t, ms.Metrics, "status")
-		require.NotNil(t, ms.Metrics, "name")
-		require.NotNil(t, ms.Metrics, "displayname")
+		if ms.Metrics["event_type"] == "flexStatusSample" {
+			continue
+		}
+		require.NotNil(t, ms.Metrics["status"], "status")
+		require.NotNil(t, ms.Metrics["name"], "name")
+		require.NotNil(t, ms.Metrics["displayname"], "displayname")
 	}
 
 	// check for a specific service, because Flex ingests everything, even output "garbage"
