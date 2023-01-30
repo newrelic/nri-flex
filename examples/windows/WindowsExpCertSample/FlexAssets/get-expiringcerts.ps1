f4b77b73e7fe363c635049c72147d9bfdfc3885f
Nimzr <nima@beaming.digital>
2023-01-30 10:07:47 -0700

    added example for pulling local certs from a windows machine, will add a dashboard in IO

---
+++ examples/windows/WindowsExpCertSample/FlexAssets/get-expiringcerts.ps1
@@ -0,0 +1,14 @@
+$Certs = get-childitem -path cert: -recurse -Expiringindays 30 | Select-Object Issuer, NotAfter, Subject, FriendlyName
+$StartDate = (Get-Date)
+$CertsToSend = @()
+Foreach ($Cert in $Certs) {
+    $CertsObject = @{
+        ExpiringIn = New-TimeSpan -Start $StartDate -End $Cert.NotAfter
+        ExpirationDate = $Cert.NotAfter | Get-Date -Uformat %s
+        Issuer = $Cert.Issuer
+        Subject = $Cert.Subject
+        FriendlyName = $Cert.FriendlyName
+    }
+    $CertsToSend += $CertsObject
+
+}
