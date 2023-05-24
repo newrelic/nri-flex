55180fa4143cd2bb329d41476dc274818546125c
Kav. Pather <kav.pather@gmail.com>
2023-05-24 14:33:06 +1000

    Update load.go

--- internal/load/load.go
+++ internal/load/load.go
@@ -115,7 +115,7 @@ const (
 	DefaultMSSQLServer = "sqlserver"
 	DefaultMySQL       = "mysql"
 	DefaultOracle      = "oracle"
-	DefaultSQLite  	 	 = "sqlite"
+	DefaultSQLite  	   = "sqlite"
 	DefaultVertica     = "vertica"
 	DefaultJmxHost     = "127.0.0.1"
 	DefaultJmxPort     = "9999"
